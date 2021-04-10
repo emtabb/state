785002642d39d188c26e54cb63d84ce62a90e913
devnguyenduc <kalone6411@gmail.com>
2021-04-10 14:05:28 +0700

    [1.0.3] Update List Object and States Interface.

--- state.go
+++ state.go
@@ -10,8 +10,77 @@ type State interface {
 	GetLabel() []string
 	GetField(string) interface{}
 
-	ToArray() []State
+	ToStates() States
 	ToString() string
 	Drop(string) error
 	Sum() float64
+}
+
+type States interface {
+	State
+	Generate()
+	ByStates([]State)
+	Add(State)
+	Of(...State)
+	Get(int) State
+	Set(int, State)
+	Size() int
+	Remove(int)
+	Pop()
+	IsEmpty() bool
+	ToArray() []State
+	Clear()
+}
+
+type List struct {
+	States
+	data []State
+}
+
+func (list *List) Generate() {
+	list.data = make([]State, 0)
+}
+
+func (list *List) ByStates(states []State) {
+	list.data = states[:]
+}
+
+func (list *List) Add(state State) {
+	list.data = append(list.data, state)
+}
+
+func (list *List) Of(states ...State) {
+	list.data = states[:]
+}
+
+func (list *List) Get(index int) State {
+	return list.data[index]
+}
+
+func (list *List) Set(index int, state State) {
+	list.data[index] = state
+}
+
+func (list *List) Size() int {
+	return len(list.data)
+}
+
+func (list *List) Remove(index int) {
+	list.data = append(list.data[:index], list.data[index + 1:]...)
+}
+
+func (list *List) Pop() {
+	list.data = list.data[:list.Size() - 1]
+}
+
+func (list *List) IsEmpty() bool {
+	return len(list.data) == 0
+}
+
+func (list *List) ToArray() []State {
+	return list.data
+}
+
+func (list *List) Clear() {
+	list.data = []State{}
 }
